630acad1e835f7c1627bbeb7255b467db5a2f7c3
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-07-21 02:58:47 +0900

    Update osu_auto_update_status.user.js

--- src/osu_auto_update_status.user.js
+++ src/osu_auto_update_status.user.js
@@ -47,14 +47,22 @@ const strtodom = (str, attribute) => {
   }
   return stemp;
 }
+const getById = (id) => {
+  return document.getElementById(id);
+}
 
 document.getElementsByClassName("page-mode--profile-page-extra")[0].append(
   mergedom(
     [
-      strtodom(
-        "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z\"/></svg>", {
-        style: "height: 14px; fill: #ccc; margin-right: 5px;"
-      }),
+      create("div", {
+        id: "user-status-update-icon",
+        style: "height: 14px;"
+      }, 
+        strtodom(
+          "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z\"/></svg>", {
+          style: "height: 14px; fill: #ccc; margin-right: 5px;",
+        })
+      ),
       create("span", {
         id: "user-status-update-time"
       }, "15秒後に更新")
@@ -65,16 +73,26 @@ document.getElementsByClassName("page-mode--profile-page-extra")[0].append(
   )
 )
 
+var update_time = 15;
+
+const time_status_update = () => {
+  update_time--;
+  getById("user-status-update-time").innerHTML = update_time + "秒後に更新";
+};
+
+setInterval(() => {
+  time_status_update();
+}, 1000);
 
 
-var before = JSON.parse(document.querySelector("div[data-initial-data]").getAttribute("data-initial-data"));
-fetch(location.href)
-.then(res => res.text())
-.then(res => {
-  var temp = res.replace(/\n/g, "");
-  temp = temp.replace(/.+data\-initial\-data="(.+?)".+/, "$1");
-  temp = temp.replace(/&quot;/g, "\"");
-  console.log(JSON.parse(temp));
-})
+// var before = JSON.parse(document.querySelector("div[data-initial-data]").getAttribute("data-initial-data"));
+// fetch(location.href)
+// .then(res => res.text())
+// .then(res => {
+//   var temp = res.replace(/\n/g, "");
+//   temp = temp.replace(/.+data\-initial\-data="(.+?)".+/, "$1");
+//   temp = temp.replace(/&quot;/g, "\"");
+//   console.log(JSON.parse(temp));
+// })
 
 })();

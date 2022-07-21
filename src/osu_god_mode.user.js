b59defb328507aa06ae207c2715e8388c8f6f388
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-07-21 18:02:58 +0900

    Create osu_god_mode.user.js

---
+++ src/osu_god_mode.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           ゴッドモード
+// @namespace       https://osu.ppy.sh/users/22136262
+// @downloadURL     https://raw.githubusercontent.com/yuzupon1133/osu-userscript-tools/main/src/osu_god_mode.user.js
+// @updateURL       https://raw.githubusercontent.com/yuzupon1133/osu-userscript-tools/main/src/osu_god_mode.user.js
+// @version         0.0
+// @description     世界ランキング1位になれます()
+// @author          yuzupon1133
+// @match           https://osu.ppy.sh/*
+// @icon            https://www.google.com/s2/favicons?sz=64&domain=ppy.sh
+// @grant           none
+// ==/UserScript==
+
+// このユーザースクリプトの説明
+//
+// GitHub
+// https://github.com/yuzupon1133/osu-userscript-tools/blob/main/src/osu_god_mode.user.js
+
+(() => {
+  return;
+})();

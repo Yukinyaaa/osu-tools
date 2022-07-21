a39062477680d7d3a04f0263573845bf7ec719b1
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-07-22 01:09:21 +0900

    Update osu_auto_update_status.user.js

--- src/osu_auto_update_status.user.js
+++ src/osu_auto_update_status.user.js
@@ -61,6 +61,33 @@ const DOMDeleteAppend = (DOM, content) => {
   DOM.append(content);
 }
 
+// ------------------- //
+
+const json_reflect = (json) => {
+  var jtemp = getByClassName("value-display__value");
+  jtemp[0].firstChild.innerHTML = "#" + num_add_comma(set_temp(json.user.statistics).global_rank);
+  jtemp[1].firstChild.innerHTML = "#" + num_add_comma(get_temp().country_rank);
+  jtemp[2].innerHTML = json.achievements.length;
+  jtemp[3].firstChild.innerHTML = num_add_comma(get_temp().pp);
+}
+
+// ------------------- //
+
+const num_add_comma = (num) => {
+  return Integer(num).toLocaleString()
+}
+var temp_temp;
+const set_temp = (data) => {
+  temp_temp = data;
+  return data;
+}
+const get_temp = () => {
+  return temp_temp;
+}
+const sec_to_hms = (sec) => {
+  
+}
+
 const waiting_icon = strtodom(
   "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z\"/></svg>", {
   style: "height: 14px; fill: #ccc; margin-right: 5px;",
@@ -104,6 +131,7 @@ getById("user-status-detail").addEventListener("click", () => {
     getById("user-status-detail").setAttribute("title", "クリックして実行");
   } else {
     DOMDeleteAppend(getById("user-status-update-icon"), waiting_icon);
+    update_time = 15;
     getById("user-status-update-time").innerHTML = "実行中";
     getById("user-status-detail").setAttribute("title", "クリックして一時停止");
   }
@@ -124,10 +152,11 @@ const time_status_update = () => {
       fetch(location.href)
       .then(res => res.text())
       .then(res => {
-        var temp = res.replace(/\n/g, "");
-        temp = temp.replace(/.+data\-initial\-data="(.+?)".+/, "$1");
-        temp = temp.replace(/&quot;/g, "\"");
-        console.log(JSON.parse(temp));
+        var res = res.replace(/\n/g, "");
+        res = res.replace(/.+data\-initial\-data="(.+?)".+/, "$1");
+        res = res.replace(/&quot;/g, "\"");
+        console.log(JSON.parse(res));
+        json_reflect(res);
         update_time = 15;
         DOMDeleteAppend(getById("user-status-update-icon"), waiting_icon);
         getById("user-status-update-time").innerHTML = update_time + "秒後に更新";

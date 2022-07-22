07de1a6a7d2669fec6b0775a1ea82a4f3a5c7d2d
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-07-22 19:03:01 +0900

    Update osu_auto_update_status.user.js

--- src/osu_auto_update_status.user.js
+++ src/osu_auto_update_status.user.js
@@ -64,17 +64,27 @@ const DOMDeleteAppend = (DOM, content) => {
 // ------------------- //
 
 const json_reflect = (json) => {
-  var jtemp = getByClassName("value-display__value");
-  jtemp[0].firstChild.innerHTML = "#" + num_add_comma(set_temp(json.user.statistics).global_rank);
-  jtemp[1].firstChild.innerHTML = "#" + num_add_comma(get_temp().country_rank);
-  jtemp[2].innerHTML = json.achievements.length;
-  jtemp[3].firstChild.innerHTML = num_add_comma(get_temp().pp);
+  try {
+    var jtemp = getByClassName("value-display__value");
+    jtemp[0].firstChild.innerHTML = "#" + num_add_comma(set_temp(json.user.statistics).global_rank);
+    jtemp[1].firstChild.innerHTML = "#" + num_add_comma(get_temp().country_rank);
+    jtemp[2].innerHTML = json.user.user_achievements.length;
+    jtemp[3].firstChild.innerHTML = num_add_comma(round(get_temp().pp));
+    jtemp[4].firstChild.innerHTML = sec_to_dhm(get_temp().play_time);
+    jtemp[4].setAttribute("title", (get_temp().play_time/3600|0) + "時間");
+  } catch(e) {
+    is_waiting_stop = true;
+    DOMDeleteAppend(getById("user-status-update-icon"), error_icon);
+    getById("user-status-update-time").innerHTML = "エラー！";
+    getById("user-status-detail").setAttribute("title", "コンソールを確認してください");
+    throw e;
+  }
 }
 
 // ------------------- //
 
 const num_add_comma = (num) => {
-  return Integer(num).toLocaleString()
+  return Number(num).toLocaleString()
 }
 var temp_temp;
 const set_temp = (data) => {
@@ -84,21 +94,34 @@ const set_temp = (data) => {
 const get_temp = () => {
   return temp_temp;
 }
-const sec_to_hms = (sec) => {
-  
+const sec_to_dhm = (time) => {
+  return `${time/3600/24|0}d ${(time/3600|0)-(time/3600/24|0)*24}h ${time%3600/60|0}m`;
+}
+const round = (num) => {
+  return Math.round(Number(num));
+}
+const ceil = (num) => {
+  return Math.ceil(Number(num));
+}
+const floor = (num) => {
+  return Math.floor(Number(num));
 }
 
 const waiting_icon = strtodom(
   "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z\"/></svg>", {
-  style: "height: 14px; fill: #ccc; margin-right: 5px;",
+  style: "height: 14px; fill: #ccc; margin-right: 5px;"
 });
 const fetching_icon = strtodom(
   "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"><path d=\"M352 0C369.7 0 384 14.33 384 32C384 49.67 369.7 64 352 64V74.98C352 117.4 335.1 158.1 305.1 188.1L237.3 256L305.1 323.9C335.1 353.9 352 394.6 352 437V448C369.7 448 384 462.3 384 480C384 497.7 369.7 512 352 512H32C14.33 512 0 497.7 0 480C0 462.3 14.33 448 32 448V437C32 394.6 48.86 353.9 78.86 323.9L146.7 256L78.86 188.1C48.86 158.1 32 117.4 32 74.98V64C14.33 64 0 49.67 0 32C0 14.33 14.33 0 32 0H352zM111.1 128H272C282.4 112.4 288 93.98 288 74.98V64H96V74.98C96 93.98 101.6 112.4 111.1 128zM111.1 384H272C268.5 378.7 264.5 373.7 259.9 369.1L192 301.3L124.1 369.1C119.5 373.7 115.5 378.7 111.1 384V384z\"/></svg>", {
-  style: "height: 14px; fill: #ccc; margin-right: 5px;",
+  style: "height: 14px; fill: #ccc; margin-right: 5px;"
 });
 const pause_icon = strtodom(
   "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 320 512\"><path d=\"M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z\"/></svg>", {
-  style: "height: 14px; fill: #ccc; margin-right: 5px;",
+  style: "height: 14px; fill: #ccc; margin-right: 5px;"
+});
+const error_icon = strtodom(
+  "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M506.3 417l-213.3-364c-16.33-28-57.54-28-73.98 0l-213.2 364C-10.59 444.9 9.849 480 42.74 480h426.6C502.1 480 522.6 445 506.3 417zM232 168c0-13.25 10.75-24 24-24S280 154.8 280 168v128c0 13.25-10.75 24-23.1 24S232 309.3 232 296V168zM256 416c-17.36 0-31.44-14.08-31.44-31.44c0-17.36 14.07-31.44 31.44-31.44s31.44 14.08 31.44 31.44C287.4 401.9 273.4 416 256 416z\"/></svg>", {
+  style: "height: 14px; fill: #ccc; margin-right: 5px;"
 });
 
 document.getElementsByClassName("page-mode--profile-page-extra")[0].append(
@@ -140,7 +163,6 @@ getById("user-status-detail").addEventListener("click", () => {
 
 var update_time = 15;
 
-
 const time_status_update = () => {
   if(!(is_waiting_stop || is_fetching)) {
     update_time--;
@@ -156,7 +178,7 @@ const time_status_update = () => {
         res = res.replace(/.+data\-initial\-data="(.+?)".+/, "$1");
         res = res.replace(/&quot;/g, "\"");
         console.log(JSON.parse(res));
-        json_reflect(res);
+        json_reflect(JSON.parse(res));
         update_time = 15;
         DOMDeleteAppend(getById("user-status-update-icon"), waiting_icon);
         getById("user-status-update-time").innerHTML = update_time + "秒後に更新";

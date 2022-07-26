f70cc7d3d5802c206140ba852092b035c1249426
yuzupon1133 <65931718+yuzupon1133@users.noreply.github.com>
2022-07-26 20:11:32 +0900

    Update osu_bbcode_parser.js

--- extra/osu_bbcode_parser.js
+++ extra/osu_bbcode_parser.js
@@ -2,144 +2,148 @@ const bbcode_parser = (raw) => {
   raw = raw.replace(/\n/g, "<br>").replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
   const html_colors = "black|silver|gray|white|maroon|red|purple|fuchsia|green|lime|olive|yellow|navy|blue|teal|aqua|orange|aliceblue|antiquewhite|aquamarine|azure|beige|bisque|blanchedalmond|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkgrey|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|gainsboro|ghostwhite|gold|goldenrod|greenyellow|grey|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightgrey|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|limegreen|linen|magenta|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|oldlace|olivedrab|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|thistle|tomato|turquoise|violet|wheat|whitesmoke|yellowgreen|rebeccapurple";
   const tags_list = "b|i|u|s|strike|color|size|spoiler|box|spoilerbox|quote|code|centre|url|profile|list|\\*|img|youtube|audio|heading|notice";
-  const tag_check = (tag) => {
+  const tag_arg_check = (tag) => {
     if(!tag) {
       return "";
     }
-    var tag_match = new RegExp("^\\[(" + tags_list + ")(?:=(.*?))?\\](.*)\\[\\/(" + tags_list + ")\\]$");
-    if(tag.match(/^\[\*\].+$/) {
-      return tag.split(/(?:hfjfjfjfjgjgngngg\n
-    }
-    var tag_syntax_check = tag.match(tag_match);
-    if(!tag_syntax_check || tag_syntax_check[1] != tag_syntax_check[4]) {
+    var tagname, arg = null, text = null;
+    var tag_ast_match = tag.match(/^\[\*\](.*)/);
+    var tag_tag_match = tag.match(new RegExp("^\\[(" + tags_list + ")(?:=(.*?))\\](.*)\\[\\/" + tags_list + "\\]$"));
+    if(tag_ast_match) {
+      tagname = "*";
+      text = tag_ast_match[1].split(/(?:\[\*\]|\[\/list\])/)[0];
+    } else if(tag_tag_match && tag_tag_match[1] == tag_tag_match[4]) {
+      tagname = tag_tag_match[1];
+      arg = tag_tag_match[2];
+      text = tag_tag_match[3];
+    } else {
       return tag;
     }
-    var arg = tag_syntax_check[2];
-    var text = tag_syntax_check[3]
-    switch(tag_syntax_check[1]) {
+    switch (tagname) {
       case "b": 
         if(!arg) {
-          return tag.replace(tag_match, "<strong>$3</strong>");
+          return "<strong>" + text + "</strong>";
         } else {
           return tag;
         }
       case "i": 
         if(!arg) {
-          return tag.replace(tag_match, "<em>$3</em>");
+          return "<em>" + text + "</em>";
         } else {
           return tag;
         }
       case "u": 
         if(!arg) {
-          return tag.replace(tag_match, "<u>$3</u>");
+          return "<u>" + text + "</u>";
         } else {
           return tag;
         }
       case "s": 
       case "strike": 
         if(!arg) {
-          return tag.replace(tag_match, "<del>$3</del>");
+          return "<del>" + text + "</del>";
         } else {
           return tag;
         }
       case "color": 
         if(arg && arg.match(new RegExp("^(?:(?:" + html_colors + ")|#[a-fA-F0-9]{6})$"))) {
-          return tag.replace(tag_match, "<span style=\"color:$2\">$3</span>");
+          return "<span style=\"color:" + arg + "\">" + text + "</span>";
         } else {
           return tag;
         }
       case "size": 
         if(["50", "85", "100", "150"].indexOf(arg) != -1) {
-          return tag.replace(tag_match, "<span class=\"size-$2\">$3</span>");
+          return "<span class=\"size-" + arg + "\">" + text + "</span>";
         } else {
           return tag;
         }
       case "spoiler": 
         if(!arg) {
-          return tag.replace(tag_match, "<span class=\"spoiler\">$3</span>");
+          return "<span class=\"spoiler\">" + text + "</span>";
         } else {
           return tag;
         }
       case "spoilerbox": 
-        if(!arg) {
-          tag = tag.replace(tag_match, "[box=SPOILER]$3[/box]");
-          arg = true;
-        } else {
-          return tag;
-        }
+        arg = "SPOILER";
       case "box": 
         if(arg) {
-          return tag.replace(tag_match, "<div class=\"js-spoilerbox bbcode-spoilerbox\"><button class=\"js-spoilerbox__link bbcode-spoilerbox__link\" type=\"button\"><span class=\"bbcode-spoilerbox__link-icon\"></span>$2</button><div class=\"bbcode-spoilerbox__body\">$3</div></div>");
+          return "<div class=\"js-spoilerbox bbcode-spoilerbox\"><button class=\"js-spoilerbox__link bbcode-spoilerbox__link\" type=\"button\"><span class=\"bbcode-spoilerbox__link-icon\"></span>" + arg + "</button><div class=\"bbcode-spoilerbox__body\">" + text + "</div></div>";
         } else {
           return tag;
         }
       case "quote": 
         if(arg && arg.match(/^".+"$/)) {
-          return tag.replace(tag_match, "<blockquote><h4>$2 wrote:</h4>$3</blockquote>");
+          return "<blockquote><h4>" + arg + " wrote:</h4>" + text + "</blockquote>";
         } else if(arg) {
           return tag;
         } else {
-          return tag.replace(tag_match, "<blockquote>$3</blockquote>");
+          return "<blockquote>" + text + "</blockquote>";
         }
       case "code": 
         if(!arg) {
-          return tag.replace(tag_match, "<pre>$3</pre>");
+          return "<pre>" + text + "</pre>";
         } else {
           return tag;
         }
       case "centre": 
         if(!arg) {
-          return tag.replace(tag_match, "<center>$3</center>");
+          return "<center>" + text + "</center>";
         } else {
           return tag;
         }
       case "url": 
         if(arg && arg.match(/^(?:https?|ftps?):\/\/.+?\..+?.*$/) && text) {
-          return tag.replace(tag_match, "<a rel=\"nofollow\" href=\"$2\">$3</a>");
+          return "<a rel=\"nofollow\" href=\"" + arg.replaceAll("\"", "\\\"") + "\">" + text + "</a>";
         } else if(!arg && text.match(/^(?:https?|ftps?):\/\/\w+?\.\w+?.*$/)) {
-          return tag.replace(tag_match, "<a rel=\"nofollow\" href=\"$3\">$3</a>");
+          return "<a rel=\"nofollow\" href=\"" + text.replaceAll("\"", "\\\"") + "\">" + text + "</a>";
         } else {
           return tag;
         }
       case "profile": 
         if((arg && arg.match(/^\d+$/)) || text.match(/^[\w- \[\]]+$/)) {
-          return tag.replace(tag_match, "<a class=\"user-name js-usercard\" href=\"https://osu.ppy.sh/users/$3\" data-hasqtip=\"107\" aria-describedby=\"qtip-107\">$3</a>");
+          return "<a class=\"user-name js-usercard\" href=\"https://osu.ppy.sh/users/" + (arg ? arg : text) + "\" data-hasqtip=\"107\" aria-describedby=\"qtip-107\">" + text + "</a>";
         } else {
           return tag;
         }
       case "list": 
         if(arg) {
-          return tag.replace(tag_match, "<ol>$3</ol>");
+          return "<ol>" + text + "</ol>";
         } else {
-          return tag.replace(tag_match, "<ol class=\"unordered\">" + text + "</ol>";
+          return "<ol class=\"unordered\">" + text + "</ol>";
+        }
+      case "*": 
+        if(!arg) {
+          return "<li>" + text + "</li>";
+        } else {
+          return tag;
         }
       case "img": 
         if(!arg && text.match(/^(?:https?|ftps?):\/\/.+?\..+?.*$/)) {
-          return tag.replace(tag_match, "<span class=\"proportional-container js-gallery\"><span class=\"proportional-container__height\" style=\"padding-bottom:55.952380952381%\"><img class=\"proportional-container__content\" src=\"$3\" alt=\"\"></span></span>");
+          return "<span class=\"proportional-container js-gallery\"><span class=\"proportional-container__height\" style=\"padding-bottom:55.952380952381%\"><img class=\"proportional-container__content\" src=\"" + text + "\" alt=\"\"></span></span>";
         } else {
           return tag;
         }
       case "youtube": 
         if(!arg && text.match(/^[\w-]{11}$/)) {
-          return tag.replace(tag_match, "<div class=\"bbcode__video\"><iframe src=\"https://www.youtube.com/embed/$3?rel=0\" frameborder=\"0\"></iframe></div>");
+          return "<div class=\"bbcode__video\"><iframe src=\"https://www.youtube.com/embed/" + text + "?rel=0\" frameborder=\"0\"></iframe></div>";
         } else {
           return tag;
         }
       case "audio": 
         if(!arg && text.match(/^(?:https?|ftps?):\/\/.+?\..+?.*$/)) {
-          return tag.replace(tag_match, "<div class=\"audio-player js-audio--player\" data-audio-url=\"$3\" data-audio-state=\"paused\" data-audio-autoplay=\"1\" data-audio-has-duration=\"1\" data-audio-time-format=\"minute_minimal\" data-audio-over50=\"0\" style=\"--duration:&quot;0:00&quot;; --current-time:&quot;0:00&quot;; --progress:0;\"><button type=\"button\" class=\"audio-player__button audio-player__button--play js-audio--play\"><span class=\"fa-fw play-button\"></span></button><div class=\"audio-player__bar audio-player__bar--progress js-audio--seek\"><div class=\"audio-player__bar-current\"></div></div><div class=\"audio-player__timestamps\"><div class=\"audio-player__timestamp audio-player__timestamp--current\"></div><div class=\"audio-player__timestamp-separator\">/</div><div class=\"audio-player__timestamp audio-player__timestamp--total\"></div></div></div>");
+          return "<div class=\"audio-player js-audio--player\" data-audio-url=\"" + text + "\" data-audio-state=\"paused\" data-audio-autoplay=\"1\" data-audio-has-duration=\"1\" data-audio-time-format=\"minute_minimal\" data-audio-over50=\"0\" style=\"--duration:&quot;0:00&quot;; --current-time:&quot;0:00&quot;; --progress:0;\"><button type=\"button\" class=\"audio-player__button audio-player__button--play js-audio--play\"><span class=\"fa-fw play-button\"></span></button><div class=\"audio-player__bar audio-player__bar--progress js-audio--seek\"><div class=\"audio-player__bar-current\"></div></div><div class=\"audio-player__timestamps\"><div class=\"audio-player__timestamp audio-player__timestamp--current\"></div><div class=\"audio-player__timestamp-separator\">/</div><div class=\"audio-player__timestamp audio-player__timestamp--total\"></div></div></div>";
         } else {
           return tag;
         }
       case "heading": 
         if(!arg) {
-          return tag.replace(tag_match, "<h2>$3</h2>");
+          return "<h2>" + text + "</h2>";
         } else {
           return tag;
         }
       case "notice": 
         if(!arg) {
-          return tag.replace(tag_match, "<div class=\"well\">$3</div>");
+          return "<div class=\"well\">" + text + "</div>";
         } else {
           return tag;
         }
@@ -163,7 +167,7 @@ const bbcode_parser = (raw) => {
       var tag_end = -1;
       if(tag_i_match[1] == undefined) {
         if(tag_i_match[2] != "*") {
-          var tag_se_check = 0;
+          var tag_check = 0;
           for(let j = i + 1; j < values.length; j++) {
             if(j == values.length - 1) {
               break;
@@ -171,21 +175,21 @@ const bbcode_parser = (raw) => {
             var tag_j_match = values[j].match(tag_regexp);
             if(tag_j_match && tag_i_match[2] == tag_j_match[2]) {
               if(tag_j_match[1] == undefined) {
-                tag_se_check++;
+                tag_check++;
               } else {
-                if(tag_se_check == 0) {
+                if(tag_check == 0) {
                   tag_end = j;
                   break;
                 }
-                tag_se_check--;
+                tag_check--;
               }
             }
           }
           if(tag_end != -1) {
-            console.log(tag_check(part_of_list(values, tag_start, tag_end).join("")));
+            console.log(tag_arg_check(tag_i_match[2], tag_i_match[3], part_of_list(values, tag_start, tag_end).join("")));
           }
         } else {
-          console.log(tag_check(part_of_list(values, tag_start, values.length).join("")));
+          console.log(tag_arg_check("*", undefined, part_of_list(values, tag_start, values.length).join("")));
         }
         counter++;
       }
